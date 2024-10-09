5987cb5e6663653f32d917fc1d6b77fa2782035a
Robert Dutra <robertdutra10@hotmail.com>
2024-10-09 09:15:42 -0300

    first commit

---
+++ src/main/java/com/back_end_challenge/BackEndChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.back_end_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndChallengeApplication.class, args);
+	}
+
+}
