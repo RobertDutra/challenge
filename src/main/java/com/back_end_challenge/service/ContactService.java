5265962b2138bddadd73e424481dc20ee6a53f42
Robert Dutra <robertdutra10@hotmail.com>
2024-10-10 18:12:00 -0300

    feat: add ContactService

---
+++ src/main/java/com/back_end_challenge/service/ContactService.java
@@ -0,0 +1,26 @@
+package com.back_end_challenge.service;
+
+import com.back_end_challenge.entity.ContactForm;
+import com.back_end_challenge.exceptions.InvalidException;
+import com.back_end_challenge.exceptions.InvalidRecaptchaException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private RecptchaService recptchaService;
+
+    @Autowired
+    private ValidationService validationService;
+
+
+
+    public void submitForm(ContactForm form) throws InvalidException, InvalidRecaptchaException {
+        validationService.isInvalidName(form.getName());
+        validationService.isInvalidEmail(form.getEmail());
+        recptchaService.verifyRecaptcha(form.getGRecaptchaResponse());
+
+    }
+}
